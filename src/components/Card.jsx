37c43ade1dbe0915e84fb5ec02106461e29db110
zuhair <zowhair@gmail.com>
2022-09-02 20:20:46 +0500

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {FittedCard, DisabledCard, Card} from "../styles/Card";
 
 function Cards({fitted, disabled, children}) {
+   console.log("thiss issksd")
   return fitted ? <FittedCard data-testid="fitted-card">{children}</FittedCard> :
          disabled ? <DisabledCard data-testid="card">{children}</DisabledCard> : <Card data-testid="card">{children}</Card>;
 }
